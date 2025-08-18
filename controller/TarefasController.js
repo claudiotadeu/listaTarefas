de2967e37570bae6d8ad5138fea6f8080fdabbc4
Claudio Tadeu <claudiotadeu@gmail.com>
2025-08-18 20:40:39 -0300

    Criação de Mensagem de Alerta e Confirmacal

--- controller/TarefasController.js
+++ controller/TarefasController.js
@@ -1,12 +1,20 @@
 const Tarefa = require("../models/Tarefa");
 
+let mensagem = "";
+let tipoMensagem = "";
+
 const getAllTarefas = async (req, res) => {
   try {
+    setTimeout(() => {
+      mensagem = "";
+    }, 2000);
     const listaTarefas = await Tarefa.find();
     return res.render("index", {
       listaTarefas,
       tarefa: null,
       tarefaDelete: null,
+      mensagem,
+      tipoMensagem,
     });
   } catch (err) {
     res.status(500).send({ error: err.message });
@@ -17,11 +25,15 @@ const createTarefa = async (req, res) => {
   const tarefa = req.body;
 
   if (!tarefa.tarefa) {
+    mensagem = "Insira um texto, antes de adicionar a Tarefa!";
+    tipoMensagem = "perigo";
     return res.redirect("/");
   }
 
   try {
     await Tarefa.create(tarefa);
+    mensagem = "Tarefa criada com sucesso!";
+    tipoMensagem = "sucesso";
     return res.redirect("/");
   } catch (err) {
     res.status(500).send({ error: err.message });
@@ -33,10 +45,22 @@ const getById = async (req, res) => {
     const listaTarefas = await Tarefa.find();
     if (req.params.method == "update") {
       const tarefa = await Tarefa.findOne({ _id: req.params.id });
-      res.render("index", { tarefa, tarefaDelete: null, listaTarefas });
+      res.render("index", {
+        tarefa,
+        tarefaDelete: null,
+        listaTarefas,
+        mensagem,
+        tipoMensagem,
+      });
     } else {
       const tarefaDelete = await Tarefa.findOne({ _id: req.params.id });
-      res.render("index", { tarefa: null, tarefaDelete, listaTarefas });
+      res.render("index", {
+        tarefa: null,
+        tarefaDelete,
+        listaTarefas,
+        mensagem,
+        tipoMensagem,
+      });
     }
   } catch (err) {
     res.status(500).send({ error: err.message });
@@ -47,6 +71,9 @@ const updateOneTarefa = async (req, res) => {
   try {
     const tarefa = req.body;
     await Tarefa.updateOne({ _id: req.params.id }, tarefa);
+    mensagem = "Tarefa atualizada com sucesso!";
+    tipoMensagem = "sucesso";
+
     res.redirect("/");
   } catch (err) {
     res.status(500).send({ error: err.message });
@@ -56,6 +83,8 @@ const updateOneTarefa = async (req, res) => {
 const deleteOneTarefa = async (req, res) => {
   try {
     await Tarefa.deleteOne({ _id: req.params.id });
+    mensagem = "Tarefa apagada com sucesso!";
+    tipoMensagem = "sucesso";
     res.redirect("/");
   } catch (err) {
     res.status(500).send({ error: err.message });
--- public/script.js
+++ public/script.js
@@ -1,1 +1,10 @@
-// alert("Mensagem Alerta funcionando!")
+const fecharMensagem = document.querySelector("#fechar-mensagem");
+const mensagem = document.querySelector(".mensagem");
+
+fecharMensagem.addEventListener("click", () => {
+  mensagem.style.display = "none";
+});
+
+setTimeout(() => {
+  mensagem.style.display = "none";
+}, 5000);
