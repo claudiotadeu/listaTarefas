b754feb6e4d0e24cd266fd95825f508ed4d3348d
Claudio Tadeu <claudiotadeu@gmail.com>
2025-09-04 20:51:47 -0300

    Atualuzacao de organização

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const conexaoDb = require("./database/db");
 
 conexaoDb();
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
